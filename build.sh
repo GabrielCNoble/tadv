6942cef0b3a347ff04037fe46cad4520d392730b
Gabriel Chiarelli Noble <gabrielcnoble@gmail.com>
2019-10-15 20:40:29 -0300

    adjust build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-gcc -c -I"SDL/include/SDL2" main.c -o objects/main.o
-gcc -c vm.c -o objects/vm.o
-gcc -c scene.c -o objects/scene.o
-gcc -c interactable.c -o objects/interactable.o
-gcc -c player.c -o objects/player.o
-gcc -c dat.c -o objects/dat.o
-gcc objects/*.o -L"SDL/lib" -lSDL2 -o build/main
+gcc -c main.c -Wall -o objects/main.o
+gcc -c vm.c -Wall -o objects/vm.o
+gcc -c scene.c -Wall -o objects/scene.o
+gcc -c interactable.c -Wall -o objects/interactable.o
+gcc -c player.c -Wall -o objects/player.o
+gcc -c dat.c -Wall -o objects/dat.o
+gcc objects/*.o -lSDL2 -o build/main
