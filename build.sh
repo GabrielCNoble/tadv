25a05371371e1392934e430ca0b2e39b3e07484b
Gabriel Chiarelli Noble <gabrielcnoble@gmail.com>
2019-10-27 00:33:42 -0300

    temporary adjustments

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-gcc -c main.c -Wall -o objects/main.o
-gcc -c vm.c -Wall -o objects/vm.o
-gcc -c scene.c -Wall -o objects/scene.o
-gcc -c interactable.c -Wall -o objects/interactable.o
-gcc -c player.c -Wall -o objects/player.o
-gcc -c dat.c -Wall -o objects/dat.o
-gcc objects/*.o -o build/main
+gcc -c main.c -Wall -o main.o
+gcc -c vm.c -Wall -o vm.o
+gcc -c scene.c -Wall -o scene.o
+gcc -c interactable.c -Wall -o interactable.o
+gcc -c player.c -Wall -o player.o
+gcc -c dat.c -Wall -o dat.o
+gcc vm.o dat.o interactable.o player.o scene.o main.o -o main
